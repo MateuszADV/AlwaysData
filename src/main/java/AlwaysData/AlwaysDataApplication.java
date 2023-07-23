04f0751b82a6e197e9c7feb58fb4e854b5c8348a
MateuszADV <mateusz.adv@gmail.com>
2023-07-23 11:53:44 +0200

    ferst commit

---
+++ src/main/java/AlwaysData/AlwaysDataApplication.java
@@ -0,0 +1,13 @@
+package AlwaysData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlwaysDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlwaysDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AlwaysData/AlwaysDataApplicationTests.java
@@ -0,0 +1,13 @@
+package AlwaysData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlwaysDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
